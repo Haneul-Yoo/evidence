2156f227672b7d60c6719297cafca62e5422fa5f
Haneul-Yoo <skysky2387@gmail.com>
2021-02-03 10:44:46 +0900

    200 data

--- convert.py
+++ convert.py
@@ -15,7 +15,9 @@ def get_cmv(data):
     cnt = 0
     for row in data:
         cnt += 1
-        if cnt > 100:
+        if cnt < 100:
+            continue
+        if cnt >= 200:
             break
         book = {
             'id': row['claim_id'],
